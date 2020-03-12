562132839d5a8069c7ee1f8af1f227d0f088129d
Mimetyque <bramstolkerd@gmail.com>
2020-03-12 09:57:28 +0100

    crée une page loe

---
+++ src/app/loe/loe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loe',
+  templateUrl: './loe.component.html',
+  styleUrls: ['./loe.component.scss'],
+})
+export class LoeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
